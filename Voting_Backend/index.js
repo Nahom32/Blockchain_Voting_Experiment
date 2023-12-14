796ccbbc4d8f502c83ffda98f25036077e072b06
Nahom32 <senaynahom00@gmail.com>
2023-12-14 19:57:38 +0300

    added a sample jscode

---
+++ Voting_Backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const Web3 = require("web3");
+const app = express();
+port = 3001
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
